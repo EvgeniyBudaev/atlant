2fbb59c16d198f1af9abf283d6f9d1e30446893a
Evgeniy  Budaev <ebudaev@iflex.ru>
2020-08-31 07:17:59 +0300

    адаптив + анимация

--- app/js/main.js
+++ app/js/main.js
@@ -6,5 +6,44 @@ $(function () {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: false,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          arrows: false
+        }
+      }
+    ]
+  });
+
+  // Scroll reveal
+  window.sr = ScrollReveal();
+
+  sr.reveal('.animate-left', {
+    origin: 'left',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+  });
+
+  sr.reveal('.animate-right', {
+    origin: 'right',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+  });
+
+  sr.reveal('.animate-top', {
+    origin: 'top',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+  });
+
+  sr.reveal('.animate-bottom', {
+    origin: 'bottom',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
   });
 })
